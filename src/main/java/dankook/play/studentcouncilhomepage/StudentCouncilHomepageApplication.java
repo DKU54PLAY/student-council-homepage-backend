6fcc3f847636a0fe697214fc32ef71616b98dba6
Donggyu <a29661498@gmail.com>
2022-05-06 01:23:53 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/dankook/play/studentcouncilhomepage/StudentCouncilHomepageApplication.java
@@ -0,0 +1,13 @@
+package dankook.play.studentcouncilhomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCouncilHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCouncilHomepageApplication.class, args);
+	}
+
+}
