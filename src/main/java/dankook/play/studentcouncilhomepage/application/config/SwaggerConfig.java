e3a08d9845798f0c6d0730f02daf85ee753ca17a
Donggyu <a29661498@gmail.com>
2022-06-20 09:39:42 +0900

    feat: Swagger 설정 추가

---
+++ src/main/java/dankook/play/studentcouncilhomepage/application/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package dankook.play.studentcouncilhomepage.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("단국대학교 총학생회 홈페이지 API Document")
+                .version("1.0")
+                .build();
+    }
+}
