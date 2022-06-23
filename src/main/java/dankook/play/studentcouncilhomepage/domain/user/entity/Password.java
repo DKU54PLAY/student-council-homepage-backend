a4806e7d25942ca8072ebed345494ce4e24be992
neoChans <herryjin@naver.com>
2022-06-23 01:18:00 -0700

    feat : 비밀번호 확인 및 예외 추가

---
+++ src/main/java/dankook/play/studentcouncilhomepage/domain/user/entity/Password.java
@@ -0,0 +1,49 @@
+package dankook.play.studentcouncilhomepage.domain.user.entity;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import dankook.play.studentcouncilhomepage.domain.user.exception.InvalidPasswordException;
+import java.util.regex.Pattern;
+import javax.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = PROTECTED)
+public class Password {
+
+    private static final String REGEX = "^.*(?=^.{8,12}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$";
+
+    private String password;
+
+    public Password(String password) {
+        validate(password);
+        this.password = password;
+    }
+
+    private static void validate(String password) {
+        checkSize(password);
+        checkArgument(Pattern.matches(REGEX, password));
+    }
+
+    private static void checkSize(String password) {
+        if (password.isBlank() || password.length() < 8 || password.length() > 12) {
+            throw new IllegalArgumentException("Invalid Password");
+        }
+    }
+
+    private static void checkArgument(boolean matches) {
+        if (!matches) {
+            throw new IllegalArgumentException("Invalid Password");
+        }
+    }
+
+    public void checkSamePassword(String password) {
+        if (!this.password.equals(password)) {
+            throw new InvalidPasswordException("Incorrect Password");
+        }
+    }
+}
