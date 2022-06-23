2584257ea72120d7db888cb3243652d72f5999a1
neoChans <herryjin@naver.com>
2022-06-23 01:17:10 -0700

    fix : jwt 파라미터 및 User ->LoginRequest로 변경

--- src/main/java/dankook/play/studentcouncilhomepage/domain/auth/JwtService.java
+++ src/main/java/dankook/play/studentcouncilhomepage/domain/auth/JwtService.java
@@ -1,7 +1,10 @@
 package dankook.play.studentcouncilhomepage.domain.auth;
 
-import dankook.play.studentcouncilhomepage.domain.user.User;
-import dankook.play.studentcouncilhomepage.domain.user.dto.LoginResponse;
+import dankook.play.studentcouncilhomepage.domain.user.entity.User;
+import dankook.play.studentcouncilhomepage.domain.user.exception.NotExistException;
+import dankook.play.studentcouncilhomepage.domain.user.repository.UserRepository;
+import dankook.play.studentcouncilhomepage.domain.user.dto.response.LoginResponse;
+import dankook.play.studentcouncilhomepage.domain.user.dto.request.LoginRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +13,12 @@ import org.springframework.stereotype.Service;
 public class JwtService {
 
     private final TokenProvider tokenProvider;
+    private final UserRepository userRepository;
 
-    public LoginResponse createToken(User user) {
-        String token = tokenProvider.createToken(user.getEmail());
+    public LoginResponse createToken(LoginRequest loginRequest) {
+        String token = tokenProvider.createToken(loginRequest.getEmail());
+        User user = userRepository.findByEmail(loginRequest.getEmail())
+                .orElseThrow(NotExistException::new);
         return new LoginResponse(user, token);
     }
 
